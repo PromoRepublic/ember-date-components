dae8944ea44ecfdca9e8ad7fb5d17dce59505307
flor <Alexandr.Florinskij@yandex.ua>
2017-08-17 14:33:15 +0300

    some fix

--- addon/components/date-picker-inline.js
+++ addon/components/date-picker-inline.js
@@ -11,21 +11,37 @@ const {
 export default DatePicker.extend({
   layout,
 
-  disabled: false,
+  /**
+   * [disablesPreviosButton description]
+   * @type {Boolean}
+   * @public
+   */
+  disablesPreviosButton: false,
 
   actions: {
+
+    /**
+     * [gotoPreviousMonth description]
+     * @return {[type]} [description]
+     * @public
+     */
     gotoPreviousMonth() {
       let month = get(this, 'currentMonth');
 
       set(this, 'currentMonth', month.clone().subtract(1, 'month'));
 
       if (moment().isSame(get(this, 'currentMonth'), 'month')) {
-        set(this, 'disabled', true);
+        set(this, 'disablesPreviosButton', true);
       }
     },
 
+    /**
+     * [gotoNextMonth description]
+     * @return {[type]} [description]
+     * @private
+     */
     gotoNextMonth() {
-      set(this, 'disabled', false);
+      set(this, 'disablesPreviosButton', false);
       let month = get(this, 'currentMonth');
       set(this, 'currentMonth', month.clone().add(1, 'month'));
     }
