3adb2f483f2bc1e84f6ae663c95d52f934fa0049
flor <Alexandr.Florinskij@yandex.ua>
2017-08-17 13:33:35 +0300

    add new functionality

--- addon/components/date-picker-inline.js
+++ addon/components/date-picker-inline.js
@@ -1,6 +1,33 @@
+import Ember from 'ember';
+import moment from 'moment';
 import DatePicker from './date-picker';
 import layout from '../templates/components/date-picker-inline';
 
+const {
+  get,
+  set
+} = Ember;
+
 export default DatePicker.extend({
-  layout
+  layout,
+
+  disabled: false,
+
+  actions: {
+    gotoPreviousMonth() {
+      let month = get(this, 'currentMonth');
+
+      set(this, 'currentMonth', month.clone().subtract(1, 'month'));
+
+      if (moment().isSame(get(this, 'currentMonth'), 'month')) {
+        set(this, 'disabled', true);
+      }
+    },
+
+    gotoNextMonth() {
+      set(this, 'disabled', false);
+      let month = get(this, 'currentMonth');
+      set(this, 'currentMonth', month.clone().add(1, 'month'));
+    }
+  }
 });
